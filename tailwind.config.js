97dc7aef8a7652bd67ed1c8fc6de3497fb9012e8
Md Asshad Hossan Ibno <88267391+bugfinder404@users.noreply.github.com>
2025-02-09 21:19:31 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			colors: {
+				skyBlue: "#00abf0", // Custom color
+			},
 			backdropBlur: {
 				sm: '4px',
-			  },
-			
-			
-		  },
+			},
 		},
+	},
 	plugins: [],
-}
+};
